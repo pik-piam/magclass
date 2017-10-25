feb58ec96a85cfd206d8af888234132ce0555ce5
bodirsky <bodirsky@pik-potsdam.de>
2017-10-25 20:01:57 +0200

    improved netcdf metadata

--- R/write.magpie.ncdf.R
+++ R/write.magpie.ncdf.R
@@ -1,9 +1,8 @@
 #' @title write.magpie.ncdf
 #' @description Writes magpie object into netcdf4 file.
 #'
-#' @param x MAgPIE object. Has to be on half degree resolution.
+#' @param x MAgPIE object. Has to be on half degree resolution. If x as comments in attr, they are plotted as global attributes.
 #' @param file_path file path as provided in write.magpie
-#' @param units will be passed on as unit in netcdf
 #' @param nc_compression Only used if filetype="nc". Sets the compression
 #' level for netCDF files (default is 9). If set to an integer between 1 (least
 #' compression) and 9 (most compression), the netCDF file is written in netCDF
@@ -12,17 +11,38 @@
 #' @return netcdf file. Writes one file per year per
 #' data column. In the case that more than one year and data column is supplied
 #' several files are written with the structure filename_year_datacolumn.asc. In the case several data dimensions exist, they are saved as subcategories.
+#' 
+#' Global Attributes can be set by using the comments within the attr of X. Comments have to have the format "indicator: comment" or " indicator:comment"
 #' @author Jan Philipp Dietrich, Florian Humpenoeder, Benjamin Leon Bodirsky
 #' @seealso \code{\link{write.magpie}}
 #' 
-write.magpie.ncdf<-function(x,file_path,units="",nc_compression = 9){
+write.magpie.ncdf<-function(x,file_path,nc_compression = 9){
   if (is.null(getNames(x)) | is.null(getYears(x))) 
     stop("Year and Data name are necessary for saving to NetCDF format")
+
+  # metadata
+  comments=getComment(x)
+  indicator = substring(text = comments,first = 1, last=regexpr(pattern = ": ",text = comments)-1)
+  commentary = substring(text = comments,first = (regexpr(pattern = ": ",text = comments)+2))
+  
+  commentary<-commentary[which(indicator!="")]
+  indicator<-indicator[which(indicator!="")]
+  
+  if(!any(indicator=="unit:")) { 
+    units="not specified" 
+  } else {
+    units <- commentary[which(indicator=="unit:")]
+  }
+  
+  if(any(regexpr(pattern = " ",text = indicator)==1)) { # delete space at first place
+    indicator[regexpr(pattern = " ",text = indicator)==1]<-substring(indicator[regexpr(pattern = " ",text = indicator)==1],first = 2)
+  }
+  
   
   getNames(x)<-gsub(pattern = "\\.",replacement = "/",getNames(x))
   
   mag <- as.array(x)
-  coord <- magclass:::magclassdata$half_deg[, c("lon", "lat")]
+  coord <- magclassdata$half_deg[, c("lon", "lat")]
   NODATA <- NA
   lon <- seq(-179.75, 179.75, by = 0.5)
   lat <- seq(-89.75, 89.75, by = 0.5)
@@ -47,6 +67,7 @@ write.magpie.ncdf<-function(x,file_path,units="",nc_compression = 9){
                               lat)
   dim_time <- ncdf4::ncdim_def("time", "years", time, 
                                calendar = "standard")
+  
   ncv <- list()
   for (i in dimnames(netcdf)[[4]]) ncv[[i]] <- ncdf4::ncvar_def(i, 
                                                                 units=units, 
@@ -54,17 +75,23 @@ write.magpie.ncdf<-function(x,file_path,units="",nc_compression = 9){
                                                                 missval=NODATA, 
                                                                 prec = "double", 
                                                                 compression = nc_compression)
+  
+
+  
   if (file.exists(file_path)) 
     file.remove(file_path)
   ncf <- ncdf4::nc_create(file_path, ncv)
   cat("Saving to NetCDF format")
+  
+  for (i in 1:length(indicator)){
+    ncdf4::ncatt_put( nc=ncf, varid=0, attname=indicator[[i]], attval=commentary[i],prec="text")  
+  }
+  
   pb <- txtProgressBar(min = 0, max = dim(netcdf)[4], 
                        style = 3)
   for (i in dimnames(netcdf)[[4]]) {
-    ncdf4::ncvar_put(ncf, ncv[[i]], netcdf[, , , 
-                                           i])
-    setTxtProgressBar(pb, which(dimnames(netcdf)[[4]] == 
-                                  i))
+    ncdf4::ncvar_put(ncf, ncv[[i]], netcdf[, , , i])
+    setTxtProgressBar(pb, which(dimnames(netcdf)[[4]] ==  i))
   }
   close(pb)
   ncdf4::nc_close(ncf)
