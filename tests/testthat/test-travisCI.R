5bb744d81527697176df39fcb07cd58db1345d47
Stephen Bi <stephenb@pik-potsdam.de>
2019-01-25 14:55:52 +0100

    Fixed bug in test-travisCI.R

--- tests/testthat/test-travisCI.R
+++ tests/testthat/test-travisCI.R
@@ -1,7 +1,7 @@
-context("Travis CI integration")
-skip_if_not(identical(Sys.getenv("TRAVIS"), "true"))
 library(testthat)
 library(lucode)
+skip_if_not(identical(Sys.getenv("TRAVIS"), "true"))
+context("Travis CI integration")
 
 test_that("Validation Key is correct (for Travis CI)", {
   expect_true(lucode:::validkey()$valid)
