7deb9f8c34e19d7dc448458ca075861df862f0f9
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2019-02-22 10:52:42 +0100

    as.magpie.array does not silently convert underscores to dots in spatial elements anymore

--- tests/testthat/test-as.magpie.R
+++ tests/testthat/test-as.magpie.R
@@ -1,9 +1,18 @@
 context("Conversion Test")
 
+data("population_magpie")
+
 test_that("conversions do not affect content", {
-  data("population_magpie")
   mag <- population_magpie
   expect_identical(as.magpie(mag),mag)
   expect_equivalent(as.magpie(as.array(mag)),mag)
   expect_equivalent(as.magpie(as.data.frame(mag)),mag)
+})
+
+test_that("underscores are preserved", {
+  mag2 <- population_magpie
+  getCells(mag2) <- paste(getCells(mag2),1:ncells(mag2),sep="_")
+  arr <- as.array(mag2)
+  mag3 <- as.magpie(arr,spatial=1)
+  expect_identical(getCells(mag3),getCells(mag2))
 })
