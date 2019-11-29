1586344a4b6aa567e0a5aec0fb60591a4d68e50b
Jan Dietrich <dietrich@pik-potsdam.de>
2019-11-29 13:49:14 +0100

    bugfixes for cases with exotic set names

--- tests/testthat/test-magpie_expand.R
+++ tests/testthat/test-magpie_expand.R
@@ -3,7 +3,8 @@ library(testthat)
 context("Expansion Test")
 
 test_that("partial expansion", {
-  skip("partial expansion not yet active by default")
+  #skip("partial expansion not yet active by default")
+  options(magclass_expand_version=2.1)
   a <- new.magpie(c("AFR.1","AFR.2","EUR.1"),fill = 1)
   b <- new.magpie(c("AFR","EUR"),fill = 1)
   expect_identical(magpie_expand(b,a),a)
@@ -13,5 +14,16 @@ test_that("partial expansion", {
                     "BLA.AFR.B","BLA.EUR.B","BLUB.AFR.B","BLUB.EUR.B"),fill = 1)
   ee <- magpie_expand(e,d)
   expect_identical(ee,magpie_expand(d,ee))
+  # test for cases with missing set names
+  names(dimnames(e)) <- NULL
+  ee <- magpie_expand(e,d)
+  expect_identical(ee,magpie_expand(d,ee))
+  
+  # test case with NA set name
+  f <- new.magpie("GLO",1900,"value", fill=1)
+  g <- new.magpie("GLO",1900,"blub", fill=1)
+  names(dimnames(g))[3] <- NA
+  ff <- magpie_expand(f,g)
+  expect_identical(ff,magpie_expand(g,ff))
 })
 
