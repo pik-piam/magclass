d08071da4d5f691a3817597a51ae57a16dc27e04
Jan Dietrich <dietrich@pik-potsdam.de>
2019-12-09 13:32:08 +0100

    -getSets add now dimCode information to output and can now be manipulated via adressing a specific subdimension

--- tests/testthat/test-getSets.R
+++ tests/testthat/test-getSets.R
@@ -2,7 +2,9 @@ context("Set Manipulation Test")
 
 test_that("sets names can be overwritten", {
   x <- new.magpie("GLO.REG","y1995","BLA.BLUB",sets=c("glo.reg","year","glo.data"))
-  expect_identical(getSets(x),c("glo","reg","year","glo","data"))
+  expect_identical(getSets(x),c("d1.1"="glo","d1.2"="reg","d2.1"="year","d3.1"="glo","d3.2"="data"))
   getSets(x)[4] <- "bla"
-  expect_identical(getSets(x),c("glo","reg","year","bla","data"))
+  expect_identical(getSets(x),c("d1.1"="glo","d1.2"="reg","d2.1"="year","d3.1"="bla","d3.2"="data"))
+  getSets(x)["d1.2"] <- "region"
+  expect_identical(getSets(x),c("d1.1"="glo","d1.2"="region","d2.1"="year","d3.1"="bla","d3.2"="data"))
 })
