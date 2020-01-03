f885edce4d6c64bc02a91af3b614e52fba6b7d9f
Kristine Karstens <karstens@pik-potsdam.de>
2020-01-03 15:41:07 +0100

    add new print option for showing mag object in wide table format for dim = 3.1 x 3.2

--- R/print.magpie.R
+++ R/print.magpie.R
@@ -6,6 +6,7 @@
 #' @param x MAgPIE object
 #' @param drop argument which controls whether empty dimensions should be
 #' skipped or not.
+#' @param reshape agrument that controls tabular representation of 3rd data dimension cross tables
 #' @param ... arguments to be passed to or from other methods.
 #' @return print displays the given MAgPIE object on screen.
 #' @author Jan Philipp Dietrich
@@ -18,12 +19,41 @@
 #'   print(population_magpie[,1,])
 #' 
 #' @export
-print.magpie <- function(x, drop=TRUE, ...) {
+print.magpie <- function(x, drop=TRUE, reshape=FALSE, ...) {
+  
+  if(reshape){
+    if(drop){
+      if(all(fulldim(x)[[1]][1:2]==1) & length(fulldim(x)[[1]])==4){
+        
+        names1 <- getNames(x, dim=1)
+        names2 <- getNames(x, dim=2)
+        datadim  <- names(fulldim(x)[[2]])[3:4]
+        
+        p <- print(reshape(subset(as.data.frame(x, rev=2), select=c(datadim,".value")), 
+                           timevar=datadim[1], idvar=datadim[2],
+                           direction="wide",
+                           new.row.names = names2,
+                           varying = list(names1))[2:3])
+        
+      } else {
+        warning("reshape option can just be used with 'fulldim(x)[[1]] == 1 1 n m'. Execute with reshape=FALSE:")
+        print.magpie(x=x, drop=drop, reshape=FALSE, ...)
+      }
+      
+    } else {
+      warning("reshape option can just be used with drop=TRUE. Execute with reshape=FALSE:")
+      print.magpie(x=x, drop=drop, reshape=FALSE, ...)
+    }
+    
+  } else {
+    
     p <- print(as.array(x)[,,,drop=drop], ...)
     
-    unit <- getMetadata(x,"unit")
-    if(!is.null(unit)) {
-      factor <- ifelse(as.numeric(unit)!=1,paste0(as.character(unit),"*"),"")
-      cat("Unit: ",factor,as.character(attr(unit, "units")),"\n", sep="")
-    }
+  }
+  
+  unit <- getMetadata(x,"unit")
+  if(!is.null(unit)) {
+    factor <- ifelse(as.numeric(unit)!=1,paste0(as.character(unit),"*"),"")
+    cat("Unit: ",factor,as.character(attr(unit, "units")),"\n", sep="")
+  }
 }
