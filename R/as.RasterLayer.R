1642253574104c856ddbdcee8026680dfa7c7258
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2021-01-07 18:48:08 +0100

    further work on raster interface (incomplete)

---
+++ R/as.RasterLayer.R
@@ -0,0 +1,43 @@
+#' as.RasterLayyer
+#' 
+#' Convert magclass object to a RasterLayer object
+#' 
+#' 
+#' @param x MAgPIE object
+#' @param res spatial data resolution. If not provided it will be guessed.
+#' @return A RasterLayer object
+#' @author Jan Philipp Dietrich
+#' @export
+
+
+as.RasterLayer <- function (x, res=NULL) {  
+      warning("Still under development! Not for productive use!")
+      if (!requireNamespace("raster", quietly = TRUE)) stop("The package \"raster\" is required for conversion of raster objects!")
+
+      .guessRes <- function(xy) {
+        .tmp <- function(x) {
+          return(suppressWarnings(min(diff(sort(unique(x))))))
+        }
+        guess <- min(.tmp(xy[[1]]),.tmp(xy[[2]]))
+        if(is.infinite(guess)) guess <- 0.5
+        return(guess)
+      }
+      
+      .position <- function(xy,res,e) {
+        ncol <- (e@xmax-e@xmin)/res
+        nrow <- (e@ymax-e@ymin)/res
+        x <- (xy$x + res/2 - e@xmin)/res
+        y <- nrow - (xy$y + res/2)/res + e@ymin
+        return(x + ncol*y)
+      }
+      
+      xy <- getCoords(x)
+      if(is.null(res)) res <- .guessRes(xy)
+      rout <- raster::raster(res=res)
+      out[.position(xy,res,raster::extent(out))] <- as.vector(x)
+      return(out)
+}
+
+
+
+
---
+++ tests/testthat/test-raster.R
@@ -0,0 +1,18 @@
+context("Raster conversion tests")
+
+skip("Not yet ready")
+skip_if_not_installed("raster")
+
+test_that("raster convertion does not alter data", {
+  for(i in c(0.5,1,2)) {
+    r <- raster::raster(res=i)
+    r[85:90,175:180] <- 1:36
+    m <- as.magpie(r)
+    r2 <- as.RasterLayer(m)
+    expect_identical(r,r2)
+    m2 <- as.magpie(r2)
+    expect_identical(m,m2)
+  }
+})
+
+
