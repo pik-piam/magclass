12588b042320d8ee5d7e4dd6cf3618a75553227a
Jan Dietrich <dietrich@pik-potsdam.de>
2021-06-14 23:09:11 +0200

    fixed coding style

--- R/write.magpie.ncdf.R
+++ R/write.magpie.ncdf.R
@@ -1,126 +1,134 @@
 #' @title write.magpie.ncdf
 #' @description Writes magpie object into netcdf4 file.
 #'
-#' @param x MAgPIE object. Has to be on half degree resolution. If x as comments in attr, they are plotted as global attributes.
+#' @param x MAgPIE object. Has to be on half degree resolution. If x as comments in attr,
+#' they are plotted as global attributes.
 #' @param file file path as provided in write.magpie
 #' @param nc_compression Only used if filetype="nc". Sets the compression
 #' level for netCDF files (default is 9). If set to an integer between 1 (least
 #' compression) and 9 (most compression), the netCDF file is written in netCDF
 #' version 4 format. If set to NA, the netCDF file is written in netCDF version
 #' 3 format.
-#' @param var_style change between variable naming in nc-file; "fullname" for ungrouped name, "grouped" for variable names divided into sub-groups
-#' @param comment Vector of comments (also used for setting the unit). Comments are set as global attributes in the netcdf file. Format of comments: "indicator: comment" (e.g. "unit: Share of land area per grid cell")
+#' @param var_style change between variable naming in nc-file; "fullname" for ungrouped name,
+#' "grouped" for variable names divided into sub-groups
+#' @param comment Vector of comments (also used for setting the unit). Comments are
+#' set as global attributes in the netcdf file. Format of comments: "indicator: comment"
+#' (e.g. "unit: Share of land area per grid cell")
 #' @param verbose Boolean deciding about whether function should be verbose or not
 
 #' @return netcdf file. Writes one file per year per
 #' data column. In the case that more than one year and data column is supplied
-#' several files are written with the structure filename_year_datacolumn.asc. In the case several data dimensions exist, they are saved as subcategories.
-#' 
+#' several files are written with the structure filename_year_datacolumn.asc. In the case
+#' several data dimensions exist, they are saved as subcategories.
+#'
 #' @author Jan Philipp Dietrich, Florian Humpenoeder, Benjamin Leon Bodirsky, Stephen Bi, Kristine Karstens
 #' @seealso \code{\link{write.magpie}}
-#' 
-write.magpie.ncdf<-function(x,file,nc_compression = 9, var_style="fullname", comment=NULL, verbose=TRUE){
-  
+#'
+write.magpie.ncdf <- function(x, file, nc_compression = 9, var_style = "fullname", comment = NULL, verbose = TRUE) { # nolint
+
   if (!requireNamespace("ncdf4", quietly = TRUE)) stop("The package ncdf4 is required for writing NCDF4 files!")
-  if (is.null(getNames(x)) | is.null(getYears(x))) 
+  if (is.null(getNames(x)) | is.null(getYears(x)))
     stop("Year and Data name are necessary for saving to NetCDF format")
 
-  if(!is.null(comment)) {
+  if (!is.null(comment)) {
     metadata <- TRUE
-    indicator = substring(text = comment,first = 1, last=regexpr(pattern = ": ",text = comment)-1)
-    commentary = substring(text = comment,first = (regexpr(pattern = ": ",text = comment)+2))
-    if(any(indicator=="")){
+    indicator <- substring(text = comment, first = 1, last = regexpr(pattern = ": ", text = comment) - 1)
+    commentary <- substring(text = comment, first = (regexpr(pattern = ": ", text = comment) + 2))
+    if (any(indicator == "")) {
       warning("incomplete metadata entry provided as comment. For netcdf, format should be 'indicator: comment'")
-      tmp<-which(indicator=="")
-      indicator[tmp]<-paste0("metadata",tmp)
+      tmp <- which(indicator == "")
+      indicator[tmp] <- paste0("metadata", tmp)
     }
-    commentary<-commentary[which(indicator!="")]
-    indicator<-indicator[which(indicator!="")]
-    
-    if(!any(indicator=="unit")) { 
-      units <- "not specified" 
-    } else {
-      units <- commentary[which(indicator=="unit")]
-    }
-    
-    if(any(regexpr(pattern = " ",text = indicator)==1)) { # delete space at first place
-      indicator[regexpr(pattern = " ",text = indicator)==1]<-substring(indicator[regexpr(pattern = " ",text = indicator)==1],first = 2)
-    }
-  }  
-  
- if(var_style == "grouped") getNames(x) <- gsub(pattern = "\\.",replacement = "/",getNames(x)) # '/' will be recognized as group-seperator
- #  var_style == "fullname" do not alter '.'-seperators, which lead to fullname variable in nc-file 
- 
- mag  <- as.array(x)
- time <- as.numeric(unlist(lapply(strsplit(dimnames(mag)[[2]], "y"), function(mag) mag[2])))
- data <- dimnames(mag)[[3]]
- 
- NODATA <- NA
- lon <- seq(-179.75, 179.75, by = 0.5)
- lat <- seq(-89.75, 89.75, by = 0.5)
- if(verbose) message("Converting MAgPIE Data to 720 x 360 array")
- netcdf <- array(NODATA, dim = c(720, 360, dim(mag)[2], 
-                                 dim(mag)[3]), dimnames = list(lon, lat, time, 
-                                                               data))
- if(length(getCells(mag))==59199){
-   
-   coord <- magclassdata$half_deg[, c("lon", "lat")]
-  
- } else {
-   
-   if(!hasCoords(x)){stop("Data has to be provided with 59199 cell or in half-degree data with cell names containing coordinate information.")}
-   coord <- getItems(x, dim=c("x","y"), full=TRUE)
-   coord$lon <- as.numeric(gsub("p","\\.", coord$x))
-   coord$lat <- as.numeric(gsub("p","\\.", coord$y))
-   coord <- data.frame(coord[3:4])
-}
- 
-  if(verbose) pb <- txtProgressBar(min = 0, max = dim(mag)[1], style = 3)
-  for (i in 1:ncells(mag)) {
-    netcdf[which(coord[i, 1] == lon), which(coord[i,2] == lat), , ] <- mag[i, , , drop = FALSE]
-    if(verbose) setTxtProgressBar(pb, i)
-  }
-  if(verbose) close(pb)
-  dim_lon <- ncdf4::ncdim_def("lon", "degrees_east", lon)
-  dim_lat <- ncdf4::ncdim_def("lat", "degrees_north", lat)
-  dim_time <- ncdf4::ncdim_def("time", "years", time, calendar = "standard")
-  
-  ncv <- list()
-  for (i in dimnames(netcdf)[[4]]) ncv[[i]] <- ncdf4::ncvar_def(i, 
-                                                                units=units, 
-                                                                dim=list(dim_lon, dim_lat, dim_time), 
-                                                                missval=NODATA, 
-                                                                prec = "double", 
-                                                                compression = nc_compression)
-  
+    commentary <- commentary[which(indicator != "")]
+    indicator <- indicator[which(indicator != "")]
 
-  
-  if (file.exists(file)) 
+    if (!any(indicator == "unit")) {
+      units <- "not specified"
+    } else {
+      units <- commentary[which(indicator == "unit")]
+    }
+    beginWithSpace <- (regexpr(pattern = " ", text = indicator) == 1)
+    if (any(beginWithSpace)) { # delete space at first place
+      indicator[beginWithSpace] <- substring(indicator[beginWithSpace], first = 2)
+    }
+  }
+
+  if (var_style == "grouped") getNames(x) <- gsub(pattern = "\\.", replacement = "/", getNames(x))
+  #  var_style == "fullname" do not alter '.'-seperators, which lead to fullname variable in nc-file
+
+  mag  <- as.array(x)
+  time <- as.numeric(unlist(lapply(strsplit(dimnames(mag)[[2]], "y"), function(mag) mag[2])))
+  data <- dimnames(mag)[[3]]
+
+  nodata <- NA
+  lon <- seq(-179.75, 179.75, by = 0.5)
+  lat <- seq(-89.75, 89.75, by = 0.5)
+  if (verbose) message("Converting MAgPIE Data to 720 x 360 array")
+  netcdf <- array(nodata, dim = c(720, 360, dim(mag)[2],
+    dim(mag)[3]), dimnames = list(lon, lat, time,
+    data))
+  if (length(getCells(mag)) == 59199) {
+
+    coord <- magclassdata$half_deg[, c("lon", "lat")]
+
+  } else {
+
+    if (!hasCoords(x)) {
+      stop("Data has to be provided with 59199 cell or in half-degree data with",
+        " cell names containing coordinate information.")
+    }
+    coord <- getItems(x, dim = c("x", "y"), full = TRUE)
+    coord$lon <- as.numeric(gsub("p", "\\.", coord$x))
+    coord$lat <- as.numeric(gsub("p", "\\.", coord$y))
+    coord <- data.frame(coord[3:4])
+  }
+
+  if (verbose) pb <- txtProgressBar(min = 0, max = dim(mag)[1], style = 3)
+  for (i in 1:ncells(mag)) {
+    netcdf[which(coord[i, 1] == lon), which(coord[i, 2] == lat), , ] <- mag[i, , , drop = FALSE]
+    if (verbose) setTxtProgressBar(pb, i)
+  }
+  if (verbose) close(pb)
+  dimLon <- ncdf4::ncdim_def("lon", "degrees_east", lon)
+  dimLat <- ncdf4::ncdim_def("lat", "degrees_north", lat)
+  dimTime <- ncdf4::ncdim_def("time", "years", time, calendar = "standard")
+
+  ncv <- list()
+  for (i in dimnames(netcdf)[[4]]) ncv[[i]] <- ncdf4::ncvar_def(i,
+    units = units,
+    dim = list(dimLon, dimLat, dimTime),
+    missval = nodata,
+    prec = "double",
+    compression = nc_compression)
+
+
+
+  if (file.exists(file))
     file.remove(file)
   ncf <- ncdf4::nc_create(file, ncv)
-  if(verbose) message("Saving to NetCDF format")
-  
-  if(metadata){
-    for (i in 1:length(indicator)){
-      if(grepl("source",indicator[[i]])) {
+  if (verbose) message("Saving to NetCDF format")
+
+  if (metadata) {
+    for (i in seq_along(indicator)) {
+      if (grepl("source", indicator[[i]])) {
         char <- as.character(commentary[[i]])
-        commentary[[i]] <- paste(char,sep="",collapse="\n")
-      }else if(indicator[[i]]=="calcHistory") {
+        commentary[[i]] <- paste(char, sep = "", collapse = "\n")
+      } else if (indicator[[i]] == "calcHistory") {
         char <- as.character(as.data.frame(commentary$calcHistory)[[1]])
-        commentary[[i]] <- paste("\n",char,sep="",collapse="")
-      }else if(indicator[[i]]=="version") {
-        commentary[[i]] <- paste("\n",names(commentary[[i]]),commentary[[i]],collapse = "; ")
+        commentary[[i]] <- paste("\n", char, sep = "", collapse = "")
+      } else if (indicator[[i]] == "version") {
+        commentary[[i]] <- paste("\n", names(commentary[[i]]), commentary[[i]], collapse = "; ")
       }
-      ncdf4::ncatt_put( nc=ncf, varid=0, attname=indicator[[i]], attval=commentary[[i]],prec="text")  
+      ncdf4::ncatt_put(nc = ncf, varid = 0, attname = indicator[[i]], attval = commentary[[i]], prec = "text")
     }
   }
-  
-  
-  if(verbose) pb <- txtProgressBar(min = 0, max = dim(netcdf)[4], style = 3)
+
+
+  if (verbose) pb <- txtProgressBar(min = 0, max = dim(netcdf)[4], style = 3)
   for (i in dimnames(netcdf)[[4]]) {
     ncdf4::ncvar_put(ncf, ncv[[i]], netcdf[, , , i])
-    if(verbose) setTxtProgressBar(pb, which(dimnames(netcdf)[[4]] ==  i))
+    if (verbose) setTxtProgressBar(pb, which(dimnames(netcdf)[[4]] ==  i))
   }
-  if(verbose) close(pb)
+  if (verbose) close(pb)
   ncdf4::nc_close(ncf)
 }
