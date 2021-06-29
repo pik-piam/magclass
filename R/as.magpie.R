6a5499bd6d789fc9c5f9af5ef75af3bdd5fa4de6
Jan Dietrich <dietrich@pik-potsdam.de>
2021-06-29 23:11:40 +0200

    added tests, improved code

--- R/as.magpie.R
+++ R/as.magpie.R
@@ -118,7 +118,7 @@ setMethod("as.magpie",
     if (!is.null(d$regiospatial)) {
       for (i in d$regiospatial) {
         ntmp <- names(dimnames(x))[1]
-        if (!is.null(ntmp)) if (!is.na(ntmp)) if (names(dimnames(x))[1] == "j") names(dimnames(x))[1] <- "i.j"
+        if (!is.null(ntmp) && !is.na(ntmp) && names(dimnames(x))[1] == "j") names(dimnames(x))[1] <- "i.j"
       }
     }
 
@@ -235,45 +235,17 @@ setMethod("as.magpie",
 setMethod("as.magpie",
   signature(x = "quitte"),
   function(x, sep = ".", replacement = "_", filter = TRUE, ...) {
-    isQuitte <- function(x, warn = FALSE) {
-      # object is not formally defined as quitte class
-      if (!methods::is(x, "quitte")) return(FALSE)
+    isQuitte <- function(x) {
 
       # object is formally defined as quitte but it has to
       # be checked whether it follows all structural
       # rules of a quitte object
 
-      # are all mandatory columns included?
       mandatoryColumns <- c("model", "scenario", "region", "variable", "unit", "period", "value")
-      if (!all(mandatoryColumns %in% names(x))) {
-        if (warn) warning("Object formally defined as quitte object, but it does not contain all ",
-          "required columns (missing: ", paste(mandatoryColumns[!(mandatoryColumns %in% names(x))],
-            collapse = ", "), ")!")
-        return(FALSE)
-      }
-
-      # are all columns factors which have to be factors?
-      factorColumns <- sapply(x[c("model", "scenario", "region", "variable", "unit")], is.factor)
-      if (!all(factorColumns)) {
-        if (warn) warning("Object defined as quitte object, but there are columns not stored as factor which",
-          " actually have to be stored that way (no factor: ",
-          paste(names(factorColumns)[!factorColumns], collapse = ", "), ")!")
-        return(FALSE)
-      }
-
-      # is the value column of type numeric?
-      if (!is.numeric(x$value)) {
-        if (warn) warning("Object formally defined as quitte object, but value column is not of type numeric!")
-        return(FALSE)
-      }
-
-      # is period column of type POSIXct?
-      if (!methods::is(x$period, "POSIXct") && !is.integer(x$period)) {
-        if (warn) warning("Object defined as quitte object, but period column is neither integer nor of type POSIXct!")
-        return(FALSE)
-      }
-
-      return(TRUE)
+      factorColumns    <- c("model", "scenario", "region", "variable", "unit")
+      isQuitte <- all(mandatoryColumns %in% names(x)) && all(sapply(x[factorColumns], is.factor)) &&
+                  is.numeric(x$value) && (methods::is(x$period, "POSIXct") || is.integer(x$period))
+      return(isQuitte)
     }
 
     if (!isQuitte(x)) {
