8a1ccdcb270b818210eeb70833099c2e3eaaf1c8
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2021-08-23 15:10:30 +0200

    increased default maximum object size by one order of magnitude

--- R/sizeCheck.R
+++ R/sizeCheck.R
@@ -1,10 +1,9 @@
 #' sizeCheck
 #'
 #' Calculates expected magclass object length and checks that it stays below
-#' the limit defined with magclass_sizeLimit. This is useful to prevent
-#' out of memory errors in case of unwanted object expansions
+#' the limit defined with magclass_sizeLimit (default = 10^9). This is useful
+#' to prevent out of memory errors in case of unwanted object expansions
 #' Ignored if \code{getOption("magclass_sizeLimit")} is negative.
-#'
 #'
 #' @param dim dimensions of the current object as returned by function \code{dim}
 #' @author Jan Philipp Dietrich
@@ -13,7 +12,7 @@
 #' pop <- maxample("pop")
 #' magclass:::sizeCheck(dim(pop))
 sizeCheck <- function(dim) {
-  if (is.null(getOption("magclass_sizeLimit"))) options(magclass_sizeLimit = 10^8) #nolint
+  if (is.null(getOption("magclass_sizeLimit"))) options(magclass_sizeLimit = 10^9) #nolint
   # estimate new object size and check against size limit
   if (!is.null(getOption("magclass_sizeLimit")) && getOption("magclass_sizeLimit") > 0) {
     size <- prod(dim)
