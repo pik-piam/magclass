1e6fbe0e4de80b919ac95bac03ec5b6505c7db92
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2021-09-29 15:46:20 +0200

    version bump

--- R/ifelse.R
+++ R/ifelse.R
@@ -3,11 +3,11 @@
 setMethod("ifelse",
           signature(test = "magpie"),
           function(test, yes, no) {
-            if (!all(suppressWarnings(as.integer(test)) %in% c(0,1))) {
+            if (!all(suppressWarnings(as.integer(test)) %in% c(0, 1))) {
               stop("'test' must only contain booleans!")
             }
-            yes <- test*yes
-            no <- (1 - test)*no
+            yes <- test * yes
+            no <- (1 - test) * no
             yes[!test] <- 0
             no[test] <- 0
             return(yes + no)
