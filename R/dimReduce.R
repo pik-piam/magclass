962bd92ac9ca546c12a56d092d756a30b2f9d8ca
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2022-02-11 09:33:22 +0100

    improved documentation

--- R/dimReduce.R
+++ R/dimReduce.R
@@ -8,22 +8,29 @@
 #' @return The reduced MAgPIE object
 #' @author Jan Philipp Dietrich
 #' @seealso \code{\link{add_dimension}}
+#' @note This function has some similarities to \code{\link{collapseDim}}, but
+#' serves a different purpose. While \code{\link{collapseDim}} only removes
+#' dimensions which contain only a single element or which it is
+#' specifically told to remove, \code{\link{dimReduce}} looks whether the
+#' entries of a multi-entry dimension are all the same and removes dimensions
+#' for which this is the case. In some cases both will lead to the same result
+#' but in many other cases the results will differ.
 #' @examples
 #'
 #' # create data with 5 identical scenarios
-#' p <- add_dimension(maxample("pop"), nm = paste0("scen", 1:5))
-#' p
-#' dimReduce(p)
+#' p <- add_dimension(maxample("pop")[1:3, 1:3, ], nm = paste0("scen", 1:2))
+#' str(p)
+#' str(dimReduce(p))
 #'
 #' # set years to same value
 #' p[, , ] <- setYears(p[, 1, ], NULL)
-#' p
-#' dimReduce(p)
+#' str(p)
+#' str(dimReduce(p))
 #'
 #' # set regions to same value
 #' p[, , ] <- setCells(p[1, , ], "GLO")
-#' p
-#' dimReduce(p)
+#' str(p)
+#' str(dimReduce(p))
 #' @export
 
 dimReduce <- function(x, dim_exclude = NULL) { # nolint
