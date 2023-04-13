eb74dfdcc0b338e47300a02187bd15c402cd2388
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-04-13 14:03:21 +0200

    missing utils::

--- R/str.magpie.R
+++ R/str.magpie.R
@@ -1,11 +1,25 @@
+#' str
+#'
+#' str method for MAgPIE objects for conventient display of the structure of a magpie object.
+#'
+#' In contrast to the default str this will not show the attributes of object@.Data as these
+#' contain only a duplicate of dimnames. Also, dim is not shown, because the information it
+#' provides is implicitly included in dimnames.
+#'
+#' @param object MAgPIE object
+#' @param ... arguments to be passed to or from other methods.
+#' @author Pascal Führlich
+#' @seealso \code{\link[utils]{str}}
+#' @examples
+#' str(maxample("pop"))
 #' @export
 str.magpie <- function(object, ...) {
   cat("A magpie object (package: magclass)\n")
   cat(" @ .Data: ")
-  str(c(object@.Data)) # c() is important here to strip attributes
+  utils::str(c(object@.Data)) # c() is important here to strip attributes
   attrib <- attributes(object)
-  cat(paste(capture.output({
-    str(attrib[setdiff(names(attrib), c("class", "dim"))])
+  cat(paste(utils::capture.output({
+    utils::str(attrib[setdiff(names(attrib), c("class", "dim"))])
   })[-1], collapse = "\n"))
   cat("\n")
   return(invisible(NULL))
