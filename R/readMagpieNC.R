b59d05e5b101055d9f1cb99ce5afe351a88f5d92
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-08-14 12:29:09 +0200

    finish read/writeMagpieNC

--- R/readMagpieNC.R
+++ R/readMagpieNC.R
@@ -1,8 +1,22 @@
+#' readMagpieNC
+#'
+#' Create a magpie object from a NetCDF file using the "terra" package.
+#'
+#' \code{\link{read.magpie}} can also create a magpie object from a NetCDF file, but
+#' it is using the packages ncdf4 and raster, while this function uses terra.
+#'
+#' @param filename the name of the NetCDF file to be read
+#' @return the newly created magpie object
+#' @author Pascal Sauer
 readMagpieNC <- function(filename) {
+  if (!requireNamespace("terra", quietly = TRUE)) {
+    stop("The package \"terra\" is required!")
+  }
   x <- terra::sds(filename)
   x <- do.call(rbind, lapply(names(x), function(name) {
     years <- terra::time(x[name])
-    a <- as.data.frame(x[name], na.rm = TRUE, xy = TRUE)
+    a <- as.data.frame(x[name], na.rm = TRUE, xy = TRUE) # creates a data frame in wide format
+    # convert to long format
     a <- do.call(rbind, lapply(seq_along(years), function(j) {
       b <- a[, c(1:2, j + 2)] # always select x and y, plus a single-year column
       names(b)[3] <- "value"
