d09246855dd5edf865ff98e6b0ef56c0f84f85ea
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-09-25 09:23:21 +0200

    asInteger for commonYears

--- R/commonYears.R
+++ R/commonYears.R
@@ -4,16 +4,17 @@
 #' ignoring magclass objects with 0 or 1 years.
 #'
 #' @param ... One or more magclass objects, or alternatively a list of magclass objects.
+#' @param asInteger Passed on to getYears. If TRUE the output format is 1995 otherwise "y1995".
 #' @return The years (as returned by getYears) the input objects have in common.
 #'
 #' @author Pascal Sauer
 #' @export
-commonYears <- function(...) {
+commonYears <- function(..., asInteger = FALSE) {
   x <- list(...)
   if (length(x) == 1 && is.list(x[[1]])) {
     x <- x[[1]]
   }
   stopifnot(vapply(x, is.magpie, logical(1)))
   x <- Filter(function(i) nyears(i) > 1, x)
-  return(Reduce(intersect, lapply(x, getYears)))
+  return(Reduce(intersect, lapply(x, getYears, as.integer = asInteger)))
 }
--- tests/testthat/test-commonYears.R
+++ tests/testthat/test-commonYears.R
@@ -5,6 +5,8 @@ test_that("commonYears works", {
   expect_identical(commonYears(pop, pop, pop), getYears(pop))
   expect_identical(commonYears(pop[, 1:3, ], pop[, 2:4, ], pop[, 3:5, ]), getYears(pop)[3])
   expect_identical(commonYears(pop[, 1:3, ], pop[, 2:4, ], pop[, 3, ]), getYears(pop)[2:3])
+  expect_identical(commonYears(pop[, 1:3, ], pop[, 2:4, ], pop[, 3, ], asInteger = TRUE),
+                   getYears(pop, as.integer = TRUE)[2:3])
   expect_identical(commonYears(list(pop[, 1:3, ], pop[, 2:4, ], pop[, 3, ])), getYears(pop)[2:3])
   expect_identical(commonYears(pop[, 1:3, ], pop[, 4, ], pop[, 3, ]), getYears(pop)[1:3])
   expect_identical(commonYears(pop[, 1, ], pop[, 4, ], pop[, 3, ]), NULL)
