cd9e2e5717513c994c01347de2effae649b5af98
Patrick R <hi@patrickrein.de>
2026-08-11 10:27:12 +0200

    Improve performance of test-mplot as it was very slow sometimes

--- tests/testthat/test-mplot.R
+++ tests/testthat/test-mplot.R
@@ -4,9 +4,13 @@ test_that("maxample runs without error for all combinations of global and total"
   withr::local_pdf(NULL)
 
   for (example in c("pop", "animal", "bilateral")) {
+    px <- maxample(example)
     for (global in c(TRUE, FALSE)) {
+      # without global, mplot renders one facet per spatial item, which dominates
+      # runtime (144 facets for "bilateral"), so plot only a few of them
+      x <- if (global) px else px[1:min(4, ncells(px)), , ]
       for (total in c(TRUE, FALSE)) {
-        capture_output(expect_no_error(mplot(maxample(!!example), global = !!global, total = !!total)))
+        capture_output(expect_no_error(mplot(x, global = !!global, total = !!total)))
       }
     }
   }
