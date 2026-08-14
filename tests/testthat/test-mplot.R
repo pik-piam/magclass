9633a37734f40e06e704489817ef29603717a0e3
Patrick R <hi@patrickrein.de>
2026-08-14 12:47:25 +0200

    Simplify a comment in test-mplot

--- tests/testthat/test-mplot.R
+++ tests/testthat/test-mplot.R
@@ -6,8 +6,7 @@ test_that("maxample runs without error for all combinations of global and total"
   for (example in c("pop", "animal", "bilateral")) {
     px <- maxample(example)
     for (global in c(TRUE, FALSE)) {
-      # without global, mplot renders one facet per spatial item, which dominates
-      # runtime (144 facets for "bilateral"), so plot only a few of them
+      # only plot a few of the bilateral pairs
       x <- if (global) px else px[1:min(4, ncells(px)), , ]
       for (total in c(TRUE, FALSE)) {
         capture_output(expect_no_error(mplot(x, global = !!global, total = !!total)))
